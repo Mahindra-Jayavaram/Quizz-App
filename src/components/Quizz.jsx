49ecdcecd27dc4a617c68154f22e3f8bf8d9f30a
Mahindra Jayavaram <mahindrajayavaram@gmail.com>
2022-06-26 12:21:57 +0530

    Initial commit

---
+++ src/components/Quizz.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { useState } from 'react';
+
+export const Quizz = () => {
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [colour, setColour] = useState("");
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+            setColour("green")
+		}
+        else setColour("red")
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+                <div className='questions'>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button className={colour} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+                </div>
+                    <div className='buttonsDiv'>
+                        <button onClick={()=> {
+                            if(currentQuestion > 0)setCurrentQuestion(currentQuestion-1)}
+                        }>  Back</button>
+                        <button onClick={()=> 
+                          { 
+                            if(currentQuestion+1 < questions.length) {
+                                setCurrentQuestion(currentQuestion+1)
+                                setColour("")
+                            }
+                            else setShowScore(true);
+                          }}>  Next</button>
+                    </div>
+				</>
+			)}
+		</div>
+	);
+}
+
